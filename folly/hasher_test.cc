511411a85b0190182786074f640219463574586c
icejoywoo <icejoywoo@gmail.com>
2023-08-17 14:52:40 +0800

    add hasher impl

--- folly/hasher_test.cc
+++ folly/hasher_test.cc
@@ -2,69 +2,71 @@
 
 #include "folly/hash/Hash.h"
 
-/**
- * Robert Jenkins' reversible 32 bit mix hash function.
- *
- * @methodset jenkins
- */
-constexpr uint32_t jenkins_rev_mix32(uint32_t key) noexcept {
-  key += (key << 12); // key *= (1 + (1 << 12))
-  key ^= (key >> 22);
-  key += (key << 4); // key *= (1 + (1 << 4))
-  key ^= (key >> 9);
-  key += (key << 10); // key *= (1 + (1 << 10))
-  key ^= (key >> 2);
-  // key *= (1 + (1 << 7)) * (1 + (1 << 12))
-  key += (key << 7);
-  key += (key << 12);
-  return key;
-}
+#include "my_hasher.hpp"
 
-/**
- * Thomas Wang 64 bit mix hash function.
- *
- * @methodset twang
- */
-constexpr uint64_t twang_mix64(uint64_t key) noexcept {
-  key = (~key) + (key << 21); // key *= (1 << 21) - 1; key -= 1;
-  key = key ^ (key >> 24);
-  key = key + (key << 3) + (key << 8); // key *= 1 + (1 << 3) + (1 << 8)
-  key = key ^ (key >> 14);
-  key = key + (key << 2) + (key << 4); // key *= 1 + (1 << 2) + (1 << 4)
-  key = key ^ (key >> 28);
-  key = key + (key << 31); // key *= 1 + (1 << 31)
-  return key;
-}
-
-/**
- * Reduce two 64-bit hashes into one.
- *
- * hash_128_to_64 uses the Hash128to64 function from Google's cityhash (under
- * the MIT License).
- */
-constexpr uint64_t hash_128_to_64(
-    const uint64_t upper, const uint64_t lower) noexcept {
-  // Murmur-inspired hashing.
-  const uint64_t kMul = 0x9ddfea08eb382d69ULL;
-  uint64_t a = (lower ^ upper) * kMul;
-  a ^= (a >> 47);
-  uint64_t b = (upper ^ a) * kMul;
-  b ^= (b >> 47);
-  b *= kMul;
-  return b;
-}
-
-constexpr uint64_t hash_128_to_64(__uint128_t u) {
-    // sizeof(__uint128_t) * 8 是完整 bit 数，sizeof(__uint128_t) * 4 就是一半的 bit 数
-    auto const hi = static_cast<uint64_t>(u >> sizeof(__uint128_t) * 4);
-    auto const lo = static_cast<uint64_t>(u);
-    return hash_128_to_64(hi, lo);
-}
-
-TEST(HasherTest, Int) {
+TEST(HasherTest, Basic) {
     EXPECT_EQ(jenkins_rev_mix32(7), folly::hasher<int32_t>()(7));
     EXPECT_EQ(jenkins_rev_mix32(7), folly::hasher<int16_t>()(7));
     EXPECT_EQ(jenkins_rev_mix32(7), folly::hasher<int8_t>()(7));
     EXPECT_EQ(twang_mix64(7), folly::hasher<int64_t>()(7));
+    // std::cout << float_hasher<float>()(7) << std::endl;
+    // std::cout << float_hasher<double>{}(7) << std::endl;
+    EXPECT_EQ(float_hasher<float>()(7), folly::hasher<float>()(7));
+    EXPECT_EQ(hasher<double>()(7), folly::hasher<double>()(7));
     EXPECT_EQ(hash_128_to_64(7), folly::hasher<__uint128_t>()(7));
+
+    // std::cout << folly::hasher<int64_t>()(7) << std::endl;
+    // 9406415178646722915
+
+    // std::cout << hasher<std::pair<int, int>>{}(std::make_pair(7, 8)) << std::endl;
+    // std::cout << folly::hasher<std::pair<int, int>>()(std::make_pair(7, 8)) << std::endl;
+    // macro 中 std::pair<int, int> 范型的类型中的逗号，会导致 macro 报错
+    typedef std::pair<int, int> int_pair;
+    EXPECT_EQ(hasher<int_pair>{}({7, 8}), folly::hasher<int_pair>{}({7, 8}));
+
+    const std::string input("abcde_bcdefgh_abcdefghxxxxxxx");
+    EXPECT_EQ(hasher<std::string>{}(input), folly::hasher<std::string>{}(input));
+    // string_view
+    EXPECT_EQ(hasher<std::string_view>{}(input), folly::hasher<std::string_view>{}(input));
+
+    std::cout << "hash array: " << hashArray<int>({1, 2, 3, 4, 5}) << std::endl;
+    std::cout << "hash row: " << hashRow<int>({1, 2, 3, 4, 5}) << std::endl;
+    std::cout << "hash map: " << hashMap<int, int>({{1, 1}, {2, 2}}) << std::endl;
+}
+
+// presto hash implementation
+template<typename Int>
+long rotateLeft(Int i, int distance) {
+    const int bits = sizeof(Int) * 8;
+    distance = distance % bits;  // 确保distance在0~bits之间
+
+    // 左移distance位，将被移出的位放到右侧
+    return (i << distance) | (i >> (bits - distance));
+}
+
+// AbstractLongType::hash
+// AbstractIntType
+// SmallintType
+// TinyintType
+// DoubleType::hash -> use AbstractLongType.hash
+template<typename Int>
+long hash(Int value) {
+  // xxhash64 mix
+  return rotateLeft(value * 0xC2B2AE3D27D4EB4FL, 31) * 0x9E3779B185EBCA87L;
+}
+
+// BooleanType::hash
+// TODO: input argument type should be bool or byte?
+long hash(bool value) {
+  return value != 0 ? 1231 : 1237;
+}
+
+// LongDecimalType. decimal has its own hash implementation
+
+TEST(HasherTest, Presto) {
+    std::cout << hash(7L) << std::endl;
+    std::cout << hash(7) << std::endl;
+    // 2554626171521168346
+    std::cout << hash(true) << std::endl;
+    std::cout << hash(false) << std::endl;
 }
---
+++ folly/my_hasher.hpp
@@ -0,0 +1,252 @@
+#pragma once
+
+#include "SpookyV2.h"
+
+#include <map>
+#include <string>
+#include <vector>
+
+// copied from folly
+
+// helper
+struct to_unsigned_fn {
+  template <typename..., typename T>
+  constexpr auto operator()(T const& t) const noexcept ->
+      typename std::make_unsigned<T>::type {
+    using U = typename std::make_unsigned<T>::type;
+    return static_cast<U>(t);
+  }
+};
+constexpr to_unsigned_fn to_unsigned{};
+
+// Order-independent way to reduce multiple 64 bit hashes into a
+// single hash. Copied from folly/hash/Hash.h because this is not
+// defined in some versions of folly.
+inline uint64_t commutativeHashMix(
+    const uint64_t upper,
+    const uint64_t lower) noexcept {
+  // Commutative accumulator taken from this paper:
+  // https://www.preprints.org/manuscript/201710.0192/v1/download
+  return 3860031 + (upper + lower) * 2779 + (upper * lower * 2);
+}
+
+// This is the Hash128to64 function from Google's cityhash (available
+// under the MIT License).  We use it to reduce multiple 64 bit hashes
+// into a single hash.
+inline uint64_t hashMix(const uint64_t upper, const uint64_t lower) noexcept {
+  // Murmur-inspired hashing.
+  const uint64_t kMul = 0x9ddfea08eb382d69ULL;
+  uint64_t a = (lower ^ upper) * kMul;
+  a ^= (a >> 47);
+  uint64_t b = (upper ^ a) * kMul;
+  b ^= (b >> 47);
+  b *= kMul;
+  return b;
+}
+
+/**
+ * Robert Jenkins' reversible 32 bit mix hash function.
+ *
+ * @methodset jenkins
+ */
+constexpr uint32_t jenkins_rev_mix32(uint32_t key) noexcept {
+  key += (key << 12); // key *= (1 + (1 << 12))
+  key ^= (key >> 22);
+  key += (key << 4); // key *= (1 + (1 << 4))
+  key ^= (key >> 9);
+  key += (key << 10); // key *= (1 + (1 << 10))
+  key ^= (key >> 2);
+  // key *= (1 + (1 << 7)) * (1 + (1 << 12))
+  key += (key << 7);
+  key += (key << 12);
+  return key;
+}
+
+/**
+ * Thomas Wang 64 bit mix hash function.
+ *
+ * @methodset twang
+ */
+constexpr uint64_t twang_mix64(uint64_t key) noexcept {
+  key = (~key) + (key << 21); // key *= (1 << 21) - 1; key -= 1;
+  key = key ^ (key >> 24);
+  key = key + (key << 3) + (key << 8); // key *= 1 + (1 << 3) + (1 << 8)
+  key = key ^ (key >> 14);
+  key = key + (key << 2) + (key << 4); // key *= 1 + (1 << 2) + (1 << 4)
+  key = key ^ (key >> 28);
+  key = key + (key << 31); // key *= 1 + (1 << 31)
+  return key;
+}
+
+/**
+ * Reduce two 64-bit hashes into one.
+ *
+ * hash_128_to_64 uses the Hash128to64 function from Google's cityhash (under
+ * the MIT License).
+ */
+constexpr uint64_t hash_128_to_64(
+    const uint64_t upper, const uint64_t lower) noexcept {
+  // Murmur-inspired hashing.
+  const uint64_t kMul = 0x9ddfea08eb382d69ULL;
+  uint64_t a = (lower ^ upper) * kMul;
+  a ^= (a >> 47);
+  uint64_t b = (upper ^ a) * kMul;
+  b ^= (b >> 47);
+  b *= kMul;
+  return b;
+}
+
+constexpr uint64_t hash_128_to_64(__uint128_t u) {
+    // sizeof(__uint128_t) * 8 是完整 bit 数，sizeof(__uint128_t) * 4 就是一半的 bit 数
+    auto const hi = static_cast<uint64_t>(u >> sizeof(__uint128_t) * 4);
+    auto const lo = static_cast<uint64_t>(u);
+    return hash_128_to_64(hi, lo);
+}
+
+
+
+// float
+// copied from folly
+template <typename F>
+struct float_hasher {
+  size_t operator()(F const& f) const noexcept {
+    static_assert(sizeof(F) <= 8, "Input type is too wide");
+
+    if (f == F{}) { // Ensure 0 and -0 get the same hash.
+      return 0;
+    }
+
+    uint64_t u64 = 0;
+    memcpy(&u64, &f, sizeof(F));
+    return static_cast<size_t>(twang_mix64(u64));
+  }
+};
+
+// std::pair
+template <typename T>
+struct hasher;
+
+struct Hash {
+  template <class T>
+  constexpr size_t operator()(const T& v) const
+      noexcept(noexcept(hasher<T>()(v))) {
+    return hasher<T>()(v);
+  }
+
+  template <class T, class... Ts>
+  constexpr size_t operator()(const T& t, const Ts&... ts) const {
+    return hash_128_to_64((*this)(t), (*this)(ts...));
+  }
+
+  constexpr size_t operator()() const noexcept { return 0; }
+};
+
+
+template <typename Int>
+struct hasher {
+  constexpr size_t operator()(Int const& i) const noexcept {
+    static_assert(sizeof(Int) <= 16, "Input type is too wide");
+    /* constexpr */ if (sizeof(Int) <= 4) {
+      auto const i32 = static_cast<int32_t>(i); // impl accident: sign-extends
+      auto const u32 = static_cast<uint32_t>(i32);
+      return static_cast<size_t>(jenkins_rev_mix32(u32));
+    } else if (sizeof(Int) <= 8) {
+      auto const u64 = static_cast<uint64_t>(i);
+      return static_cast<size_t>(twang_mix64(u64));
+    } else {
+      auto const u = to_unsigned(i);
+      auto const hi = static_cast<uint64_t>(u >> sizeof(Int) * 4);
+      auto const lo = static_cast<uint64_t>(u);
+      return hash_128_to_64(hi, lo);
+    }
+  }
+};
+
+template <>
+struct hasher<float> : float_hasher<float> {};
+
+template <>
+struct hasher<double> : float_hasher<double> {};
+
+template <typename T1, typename T2>
+struct hasher<std::pair<T1, T2>> {
+  size_t operator()(const std::pair<T1, T2>& key) const {
+    return Hash()(key.first, key.second);
+  }
+};
+
+template <>
+struct hasher<bool> {
+  using folly_is_avalanching = std::true_type;
+
+  constexpr size_t operator()(bool key) const noexcept {
+    // Make sure that all the output bits depend on the input.
+    return key ? std::numeric_limits<size_t>::max() : 0;
+  }
+};
+
+
+// string
+template <>
+struct hasher<std::string> {
+  size_t operator()(const std::string& key) const {
+    return static_cast<size_t>(
+        SpookyHash::Hash64(key.data(), key.size(), 0));
+  }
+};
+
+template <>
+struct hasher<std::string_view> {
+  size_t operator()(const std::string_view& key) const {
+    return static_cast<size_t>(
+        SpookyHash::Hash64(key.data(), key.size(), 0));
+  }
+};
+
+// complex type hash implementation
+static constexpr uint64_t nullHash = 1;
+template <typename T>
+uint64_t hashArray(
+    uint64_t hash,
+    const std::vector<T>& elements) {
+  for (auto i = 0; i < elements.size(); ++i) {
+    auto elementHash = hasher<T>{}(elements[i]);
+    hash = commutativeHashMix(hash, elementHash);
+  }
+  return hash;
+}
+
+template <typename T>
+uint64_t hashArray(
+    const std::vector<T>& elements) {
+  return hashArray(nullHash, elements);
+}
+
+template <typename Key, typename Value>
+uint64_t hashMap(
+    const std::map<Key, Value>& elements) {
+  std::vector<Key> keys(elements.size());
+  std::vector<Value> values(elements.size());
+  for (auto& [key, value] : elements) {
+    keys.emplace_back(key);
+    values.emplace_back(value);
+  }
+  return hashArray(hashArray(nullHash, keys), values);
+}
+
+template <typename T>
+uint64_t hashRow(
+    const std::vector<T>& elements) {
+  uint64_t hash = nullHash;
+  bool isFirst = true;
+  for (auto i = 0; i < elements.size(); ++i) {
+    auto element = elements[i];
+    if (element) {
+      auto elementHash = hasher<T>{}(element);
+      hash = isFirst ? elementHash : hashMix(hash, elementHash);
+      isFirst = false;
+    }
+  }
+  return hash;
+}
+
