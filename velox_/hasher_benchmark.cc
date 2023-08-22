fb5856a57e291c8281530cae9d1491b36b839b5a
icejoywoo <icejoywoo@gmail.com>
2023-08-22 22:43:54 +0800

    update

--- velox_/hasher_benchmark.cc
+++ velox_/hasher_benchmark.cc
@@ -5,20 +5,22 @@
 #include "presto_hasher.hpp"
 
 /// https://github.com/facebook/folly/blob/main/folly/docs/Benchmark.md
+/// https://stackoverflow.com/questions/1152333/force-compiler-to-not-optimize-side-effect-less-statements
+static volatile uint64_t result = 0;
 BENCHMARK(follyHasher, n) {
     for (unsigned int i = 0; i < n; ++i) {
-        folly::hasher<int>{}(i);
+        result = folly::hasher<int>{}(i);
     }
 }
 BENCHMARK_RELATIVE(veloxHasher, n) {
     for (unsigned int i = 0; i < n; ++i) {
-        velox::hash::hasher<int>{}(i);
+        result = velox::hash::hasher<int>{}(i);
     }
 }
 
 BENCHMARK_RELATIVE(prestoHasher, n) {
     for (unsigned int i = 0; i < n; ++i) {
-        presto::hash::hasher<int>{}(i);
+        result = presto::hash::hasher<int>{}(i);
     }
 }
 
