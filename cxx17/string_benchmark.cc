d62356798d0cc18705cbc4e62408423916341a5e
icejoywoo <icejoywoo@gmail.com>
2023-09-06 20:21:37 +0800

    update

--- cxx17/string_benchmark.cc
+++ cxx17/string_benchmark.cc
@@ -3,6 +3,7 @@
 #include <string>
 #include <string_view>
 #include <random>
+#include <vector>
 
 std::string generateRandomString(int length, unsigned seed) {
   std::random_device rd;
@@ -33,6 +34,48 @@ BENCHMARK_RELATIVE(string_view_substr, n) {
 }
 
 BENCHMARK_DRAW_LINE();
+
+constexpr int factor = 1000000;
+
+BENCHMARK(string_without_reserve, n) {
+  std::string result;
+  size_t size = n * factor;
+  for (unsigned int i = 0; i < size; ++i) {
+    result.push_back(i);
+    folly::doNotOptimizeAway(result);
+  }
+}
+
+BENCHMARK_RELATIVE(string_with_reserve, n) {
+  std::string result;
+  size_t size = n * factor;
+  result.reserve(size);
+  for (unsigned int i = 0; i < size; ++i) {
+    result.push_back(i);
+    folly::doNotOptimizeAway(result);
+  }
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(vector_without_reserve, n) {
+  std::vector<unsigned int> result;
+  size_t size = n * factor;
+  for (unsigned int i = 0; i < size; ++i) {
+    result.push_back(i);
+    folly::doNotOptimizeAway(result);
+  }
+}
+
+BENCHMARK_RELATIVE(vector_with_reserve, n) {
+  size_t size = n * factor;
+  std::vector<unsigned int> result;
+  result.reserve(size);
+  for (unsigned int i = 0; i < size; ++i) {
+    result.push_back(i);
+    folly::doNotOptimizeAway(result);
+  }
+}
 
 int main() {
   // init input strings
